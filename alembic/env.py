26817b938b71ea9f3a878f3a23ab7626bedcdbfa
Teknetron <hrgumroyan@gmail.com>
2026-01-17 01:04:37 +0400

    add database configuration and initial migration

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,10 @@
 from logging.config import fileConfig
 
+import os
+from dotenv import load_dotenv
+from src.marketing_messaging_service.infrastructure.database import Base
+from src.marketing_messaging_service import models
+
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -8,6 +13,8 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+load_dotenv()
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +25,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,7 +46,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = os.environ.get("DATABASE_URL") or config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,8 +65,11 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    configuration = config.get_section(config.config_ini_section) or {}
+    configuration["sqlalchemy.url"] = os.environ.get("DATABASE_URL") or configuration.get("sqlalchemy.url")
+
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
